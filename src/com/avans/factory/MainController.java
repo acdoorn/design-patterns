0faed8b581c546618d7080cb0fe2b4e008a0e32e
Ruud van Daelen <rdaelen@student.avans.nl>
2015-06-10 15:14:52 +0200

    maincontroller en nodefactory

---
+++ src/com/avans/factory/MainController.java
@@ -0,0 +1,25 @@
+package com.avans.factory;
+
+public class MainController {
+	private NodeReader reader = new NodeReader();
+	private NodeFactory factory = new NodeFactory();
+	//private Array[] circuit;
+	
+	//Constructor
+	public MainController() {
+		
+	}
+	
+	//Methods
+	public void readFile(String path) {
+		reader.readFile(path);
+	}
+	
+	public void generate() {
+		
+	}
+	
+	public void execute() {
+		
+	}
+}
---
+++ src/com/avans/factory/NodeFactory.java
@@ -0,0 +1,19 @@
+package com.avans.factory;
+
+public class NodeFactory {
+	
+	//Constructor
+	public NodeFactory() {
+		
+	}
+	
+	//Methods
+	public Node createNode(String nodeName, String portType) {
+		
+	}
+	
+	public Node addEdge(String nodeName, String edgeName) {
+		
+	}
+	
+}
